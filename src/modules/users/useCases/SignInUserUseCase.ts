82769247374deca1c41381dffae6da362bc473fe
Matías Capuano <maticapuano97@gmail.com>
2021-11-24 20:53:36 -0300

    refactor(signin): implement logic validate password

--- src/modules/users/useCases/SignInUserUseCase.ts
+++ src/modules/users/useCases/SignInUserUseCase.ts
@@ -1,7 +1,9 @@
 import { inject, injectable } from "tsyringe";
 import { userConstants } from "../../../shared/core/constants/users.constant";
 import { UseCase } from "../../../shared/core/UseCase";
+import { BadRequestError } from "../../../shared/errors/BadRequestError";
 import { NotFoundError } from "../../../shared/errors/NotFoundError";
+import { HashProvider } from "../../../shared/providers/HashProvider/interfaces/HashProvider";
 import { JwtProvider } from "../../../shared/providers/JwtProvider/interfaces/JwtProvider";
 import { ISignInUser } from "../domain/ISignInUser";
 import { IUserWithToken } from "../domain/IUserWithToken";
@@ -12,14 +14,21 @@ export class SignInUserUseCase implements UseCase<ISignInUser, IUserWithToken> {
   public constructor(
     @inject("UsersRepository") private _usersRepository: IUsersRepository,
     @inject("JwtProvider") private _jwtProvider: JwtProvider,
+    @inject("HashProvider") private _hashProvider: HashProvider,
   ) {}
 
   public async execute(data: ISignInUser): Promise<IUserWithToken> {
-    const { email } = data;
+    const { email, password } = data;
     const existsEmail = await this._usersRepository.findByEmail(email);
 
     if (!existsEmail) {
-      throw new NotFoundError(userConstants.USER_NOT_FOUND);
+      throw new BadRequestError(userConstants.EMAIL_OR_PASSWORD_INVALID);
+    }
+
+    const passwordMatched = await this._hashProvider.compare(password, existsEmail.password);
+
+    if (!passwordMatched) {
+      throw new BadRequestError(userConstants.EMAIL_OR_PASSWORD_INVALID);
     }
 
     const { full_name, id: user_id } = existsEmail;
--- src/shared/core/constants/users.constant.ts
+++ src/shared/core/constants/users.constant.ts
@@ -1,4 +1,5 @@
 export const userConstants = {
   EMAIL_ALREADY_EXISTS: "Email already exists",
   USER_NOT_FOUND: "User not found",
+  EMAIL_OR_PASSWORD_INVALID: "Email or password invalid",
 };
