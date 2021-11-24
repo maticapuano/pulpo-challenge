91e52223a785eed52a4979c30bccbc988f539845
Matías Capuano <maticapuano97@gmail.com>
2021-11-24 20:52:47 -0300

    test(signin): add test when user signin

---
+++ src/modules/users/useCases/SignInUserUseCase.spec.ts
@@ -0,0 +1,73 @@
+import { BadRequestError } from "../../../shared/errors/BadRequestError";
+import { NotFoundError } from "../../../shared/errors/NotFoundError";
+import { FakeHashProvider } from "../../../shared/providers/HashProvider/fake/FakeHashProvider";
+import { JwtProvider } from "../../../shared/providers/JwtProvider/interfaces/JwtProvider";
+import { UsersFakeRepository } from "../infra/typeorm/repositories/fake/UsersFakeRepository";
+import { SignInUserUseCase } from "./SignInUserUseCase";
+
+describe("SignInUserUseCase", () => {
+  let fakeHashProvider: FakeHashProvider;
+  let fakeUsersRepository: UsersFakeRepository;
+  let fakeJwtProvider: JwtProvider;
+  let signInUserUseCase: SignInUserUseCase;
+
+  beforeEach(() => {
+    fakeHashProvider = new FakeHashProvider();
+    fakeUsersRepository = new UsersFakeRepository();
+    fakeJwtProvider = {
+      createAccessToken: jest.fn().mockImplementation(() => Promise.resolve("some-token")),
+      decodeAccessToken: jest.fn().mockImplementation(() =>
+        Promise.resolve({
+          user_id: "some-id",
+          full_name: "John Doe",
+          email: "john@doe.com",
+        }),
+      ),
+    };
+    signInUserUseCase = new SignInUserUseCase(
+      fakeUsersRepository,
+      fakeJwtProvider,
+      fakeHashProvider,
+    );
+  });
+
+  it("should be able to authenticate", async () => {
+    const user = await fakeUsersRepository.create({
+      full_name: "John Doe",
+      email: "john@doe.com",
+      password: "123456",
+    });
+
+    const response = await signInUserUseCase.execute({
+      email: "john@doe.com",
+      password: "123456",
+    });
+
+    expect(response.user).toEqual(user);
+    expect(response.access_token).toBe("some-token");
+  });
+
+  it("should not be able to authenticate with non-existing user", async () => {
+    await expect(
+      signInUserUseCase.execute({
+        email: "john@doe.com",
+        password: "123456",
+      }),
+    ).rejects.toBeInstanceOf(BadRequestError);
+  });
+
+  it("should not be able to authenticate with wrong password", async () => {
+    await fakeUsersRepository.create({
+      full_name: "John Doe",
+      email: "john@doe.com",
+      password: "123456",
+    });
+
+    await expect(
+      signInUserUseCase.execute({
+        email: "johndoe@example.com",
+        password: "wrong-password",
+      }),
+    ).rejects.toBeInstanceOf(BadRequestError);
+  });
+});
