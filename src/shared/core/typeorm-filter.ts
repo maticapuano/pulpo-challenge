f0d7f13366bed6697c56f071de695cbe755e1af1
Matías Capuano <maticapuano97@gmail.com>
2021-11-24 19:57:40 -0300

    chore(helpers): add filter helper Typeorm

---
+++ src/shared/core/typeorm-filter.ts
@@ -0,0 +1,24 @@
+import { FindManyOptions, Like, Repository } from "typeorm";
+
+export type ExpressQuery<T = any> = Record<keyof T, any>;
+
+export const typeormFilterHelper = async <T = any>(
+  repository: Repository<T>,
+  query?: Partial<ExpressQuery<T>>,
+) => {
+  const columns = repository.metadata.columns.map((column) => column.propertyName);
+  const options: FindManyOptions<T> = {
+    where: {},
+  };
+
+  if (query) {
+    Object.keys(query).forEach((key) => {
+      if (columns.includes(key)) {
+        //@ts-ignore
+        options.where[key] = Like(`%${query[key]}%`);
+      }
+    });
+  }
+
+  return await repository.find(options);
+};
