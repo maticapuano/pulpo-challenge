19dbe5c29e1a854aadf4c14e98b48f311fd5e800
Matías Capuano <maticapuano97@gmail.com>
2021-11-24 10:30:17 -0300

    refactor(signin): add validation when a user signin request

--- src/modules/users/infra/http/controllers/SignInUserController.ts
+++ src/modules/users/infra/http/controllers/SignInUserController.ts
@@ -1,12 +1,15 @@
 import { Request, Response } from "express";
 import { container } from "tsyringe";
+import { yupValidationSchema } from "../../../../../shared/core/validation/yup-validation-schema";
+import { ISignInUser } from "../../../domain/ISignInUser";
 import { SignInUserUseCase } from "../../../useCases/SignInUserUseCase";
+import { signInUserValidation } from "../../../validation/signin-user-validation";
 
 export class SignInUserController {
   public async handle(req: Request, res: Response): Promise<Response> {
     const useCase = container.resolve(SignInUserUseCase);
-    //TODO: implemented validation request
-    const user = await useCase.execute(req.body);
+    const data: ISignInUser = await yupValidationSchema(signInUserValidation, req.body);
+    const user = await useCase.execute(data);
 
     return res.status(200).json(user);
   }
